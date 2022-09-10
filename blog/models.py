07fd1a236721b7b250fe91a338aa86597d42419f
FatihG34 <fthgnydn34@gmail.com>
2022-09-10 11:21:38 +0300

    post model modified category added status  added

--- blog/models.py
+++ blog/models.py
@@ -5,15 +5,28 @@ from django.utils.text import slugify
 # Create your models here.
 
 
+class Category(models.Model):
+    name = models.CharField('Category Title', max_length=50)
+    # post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
+    STATUS = [
+        ('Draft', 'Draft'),
+        ('Published', 'Published'),
+    ]
     title = models.CharField('Title', max_length=50)
     content = models.TextField('Content')
     image = models.URLField('Image URL', max_length=200)
     publish_date = models.DateTimeField('Publish Date', auto_now_add=True)
     last_updated = models.DateTimeField('Last Updated Date', auto_now=True)
-    status = models.CharField('Status', max_length=50)
+    status = models.CharField('Status', max_length=50, choices=STATUS)
     slug = models.SlugField(null=False, unique=True, db_index=True, blank=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
@@ -22,27 +35,24 @@ class Post(models.Model):
         self.slug = slugify(self.title)
         super().save(*args, **kwargs)
 
-class Category(models.Model):
-    name= models.CharField('Category Title', max_length=50)
-    post = models.ForeignKey(Post,on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
 
 class Comment(models.Model):
     content = models.TextField()
-    time_stamp = models.DateTimeField('Time, comment added', auto_now_add=False)
+    time_stamp = models.DateTimeField(
+        'Time, comment added', auto_now_add=False)
     postcomment = models.ForeignKey(Post, on_delete=models.CASCADE)
     post_user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'Added comment at {self.time_stamp} '
 
+
 class Like(models.Model):
     like = models.BooleanField('Like')
     post_like = models.ForeignKey(Post, on_delete=models.CASCADE)
-    like_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    like_user = models.OneToOneField(User, on_delete=models.CASCADE)
     
+
 
 class Post_view(models.Model):
     view_time = models.DateTimeField('View Time', auto_now_add=False)
