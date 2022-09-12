ca9fa927404ad2de1836b20b1333e22b70ca06b6
FatihG34 <fthgnydn34@gmail.com>
2022-09-12 16:59:48 +0300

    modified

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from blog.views import comment, create_post, home, update_post
+from blog.views import comment, create_post, home, update_post, detail
 
 app_name='blog'
 urlpatterns = [
     path('', home,name='home'),
     path("create/", create_post, name='create'),
-    path("update/<int:id>", update_post, name='update'),
-    path("comment/<int:id>", comment, name='comment'),
+    path("<str:slug>/", detail, name='detail'),
+    path("<str:slug>/update/", update_post, name='update'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,19 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from blog.forms import BlogForm, CommentForm
 from django.contrib import messages
 from blog.models import Comment, Post
 
 # Create your views here.
+
+
 def home(request):
     blogs = Post.objects.all()
     context = {
         'blogs': blogs,
     }
     return render(request, 'blog/post_list.html', context)
+
 
 @login_required(login_url='login')
 def create_post(request):
@@ -23,37 +26,44 @@ def create_post(request):
             blog.save()
             messages.success(request, "Blog created successfully 👏🏻")
             redirect('blog:home')
-    context={
+    context = {
         'form': form
     }
     return render(request, 'blog/post_create.html', context)
 
 
 @login_required(login_url='login')
-def update_post(request,id):
-    blogpost = Post.objects.get(id=id)
-    form = BlogForm(instance=blogpost)
-    if request.method =='POST':
-        form = BlogForm(request.POST, instance=blogpost)
-        if form.is_valid():
-            form.save()
-            return redirect("home")
-    context={
+def detail(request, slug):
+    objct = Post.objects.get(slug=slug)
+    context = {
+        'post_object': objct
+    }
+    return render(request, 'blog/detail.html', context)
+
+
+@login_required(login_url='login')
+def update_post(request, slug):
+    blogpost = Post.objects.get(slug=slug)
+    form = BlogForm(request.POST or None, request.FILES or None, instance=blogpost)
+    if form.is_valid():
+        form.save()
+        return redirect("home")
+    context = {
         'blogpost': blogpost,
-        'update_form':form
+        'update_form': form
     }
     return render(request, 'blog/post_update.html', context)
 
-def comment(request,id):
-    blogpost = Post.objects.get(id=id)
-    print(blogpost.id)
+
+def comment(request,  slug):
+    blogpost = Post.objects.get(slug=slug)
     comment_post = Comment.objects.filter(id=blogpost.id)
     comment_form = CommentForm()
     if request.method == 'POST':
         comment_form = CommentForm(request.POST)
         if comment_form.is_valid():
             comment_form.save()
-            
+
     context = {
         'blogpost': blogpost,
         'comment_post': comment_post,
