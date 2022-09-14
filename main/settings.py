3169ee0abed3fbbc8cacb5049dd7a41151e2607d
FatihG34 <fthgnydn34@gmail.com>
2022-09-14 19:17:34 +0300

    modified

--- main/settings.py
+++ main/settings.py
@@ -132,9 +132,8 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-MEDIA_ROOT = BASE_DIR / 'media/'
-
 MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 LOGIN_REDIRECT_URL = 'home'
 
--- users/models.py
+++ users/models.py
@@ -2,7 +2,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, AbstractUser
 # Create your models here.
 
+
 class User(AbstractUser):
-    profile_pic = models.ImageField('Profile Picture', upload_to='profile_pics', blank=True)
+    profile_pic = models.ImageField(
+        'Profile Picture', upload_to='profile_pics', default="media\Avatar.png", blank=True)
     bio = models.TextField('Bio', blank=True)
     
